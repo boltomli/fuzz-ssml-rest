1d74df566ad9d2673d2d8c71753f75a34866be0c
Song Li <songl@outlook.com>
2020-03-03 18:05:11 +0800

    Add basic tests with args

--- src/test/kotlin/fuzz/ssml/rest/AppTest.kt
+++ src/test/kotlin/fuzz/ssml/rest/AppTest.kt
@@ -8,6 +8,22 @@ import kotlin.test.*
 class AppTest {
     @Test fun testSSML() {
         assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "<speak version=\"1.0\" xml:lang=\"en-us\"><voice xml:lang=\"en-us\" xml:gender=\"Female\" name=\"en-US-JessaNeural\">1: 23456. 789? 0!</voice></speak>", ssml())
+                "<speak version=\"1.0\" xml:lang=\"en-us\"><voice xml:lang=\"en-us\" xml:gender=\"Female\" name=\"en-US-JessaNeural\">1: 23456. 789? 0!</voice></speak>",
+                ssml())
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<speak version=\"1.0\" xml:lang=\"en-us\"><voice xml:lang=\"en-us\" xml:gender=\"Female\" name=\"en-US-ZiraRUS\">1: 23456. 789? 0!</voice></speak>",
+                ssml(name = "en-US-ZiraRUS"))
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<speak version=\"1.0\" xml:lang=\"en-us\"><voice xml:lang=\"en-us\" xml:gender=\"Female\" name=\"en-US-JessaNeural\">abcdefg</voice></speak>",
+                ssml(text = "abcdefg"))
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<speak version=\"1.0\" xml:lang=\"pt-br\"><voice xml:lang=\"pt-br\" xml:gender=\"Female\" name=\"pt-BR-FranciscaNeural\">1: 23456. 789? 0!</voice></speak>",
+                ssml(lang = "pt-br", name = "pt-BR-FranciscaNeural"))
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<speak version=\"1.0\" xml:lang=\"en-us\"><voice xml:lang=\"en-us\" xml:gender=\"Male\" name=\"en-US-GuyNeural\">1: 23456. 789? 0!</voice></speak>",
+                ssml(gender = "Male", name = "en-US-GuyNeural"))
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<speak version=\"1.0\" xml:lang=\"zh-cn\"><voice xml:lang=\"zh-cn\" xml:gender=\"Male\" name=\"zh-CN-YunyangNeural\">并非所有服务都适用于所有市场。</voice></speak>",
+                ssml(lang = "zh-cn", gender = "Male", name = "zh-CN-YunyangNeural", text = "并非所有服务都适用于所有市场。"))
     }
 }
